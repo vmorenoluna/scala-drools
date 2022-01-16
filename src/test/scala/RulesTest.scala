13ff570d8ad0f3d604af9775738ab8f5a0721bf5
vincenzo <morenolunav@gmail.com>
2022-01-16 19:28:14 +0000

    Completed the example

--- src/test/scala/RulesTest.scala
+++ src/test/scala/RulesTest.scala
@@ -33,61 +33,12 @@ class RulesTest extends AnyFlatSpec {
     statefulSession.fireAllRules()
     customer.setAge(20)
     statefulSession.update(factHandle, customer)
+    statefulSession.fireAllRules()
     statefulSession.delete(factHandle)
     statefulSession.fireAllRules()
 
     val n = 1
     n shouldEqual 1
   }
-
-//  "session" should "work with emotion rule" in {
-//
-//    val statefulSession = KnowledgeSessionHelper.getStatefulKnowledgeSession(kieContainer, "rule-session")
-//    statefulSession.addEventListener(
-//      new RuleRuntimeEventListener {
-//        override def objectInserted(event: ObjectInsertedEvent): Unit =
-//          println(s"Object inserted: ${event.getObject.toString}")
-//
-//        override def objectUpdated(event: ObjectUpdatedEvent): Unit =
-//          println(s"Object updated: ${event.getObject.toString}")
-//
-//        override def objectDeleted(event: ObjectDeletedEvent): Unit =
-//          println(s"Object deleted: ${event.getOldObject.toString}")
-//      }
-//    )
-//    statefulSession.setGlobal("logger", new RulesLogger)
-//    val map: mutable.Map[Point, Interval] = mutable.Map()
-//    statefulSession.setGlobal("map", map)
-//    val emotion: Emotion = Emotion(EmotionName.JOYFUL, 0.5, 1.0)
-//    statefulSession.insert(emotion)
-//    val point: Point = Point(0.5, 0.5)
-//    statefulSession.insert(point)
-//    statefulSession.fireAllRules()
-//
-//    val objects = statefulSession.getObjects(new ClassObjectFilter(classOf[Emotion]))
-//    objects.size shouldEqual 1
-//
-//    val i = objects.iterator()
-//    val e = i.next().asInstanceOf[Emotion]
-//    e shouldEqual (emotion)
-//  }
-//
-//  "session" should "should pass the third test" in {
-//
-//    val statefulSession = KnowledgeSessionHelper.getStatefulKnowledgeSessionWithCallback(kieContainer, "rule2-session")
-//    //    statefulSession.setGlobal("logger", new RulesLogger)
-//    val emotion: Emotion = Emotion(EmotionName.JOYFUL, 1.0, 1.0)
-//    statefulSession.insert(emotion)
-//    val point: Point = Point(1.0, 1.0)
-//    statefulSession.insert(point)
-//    statefulSession.fireAllRules()
-//
-//    val objects = statefulSession.getObjects(new ClassObjectFilter(classOf[Emotion]))
-//    objects.size shouldEqual 1
-//
-//    val i = objects.iterator()
-//    val e = i.next().asInstanceOf[Emotion]
-//    e shouldEqual (emotion)
-//  }
 
 }
